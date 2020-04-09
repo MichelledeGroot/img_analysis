ffa628b6e096ebf450c57a97d6242e3c9c4c261d
michelle <M.deGroot5@student.han.nl>
2020-04-09 11:44:28 +0200

    added pydoc

--- src/filter_diseases.py
+++ src/filter_diseases.py
@@ -8,11 +8,20 @@ ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..'))
 directory = f'{ROOT_DIR}'
 original_dataset_dir = f'/NAS/NIH_chest/ChestXray-NIHCC/images/'
 data_entry = os.path.join(directory, 'Data_Entry_2017.csv')
-print(directory)
-print(original_dataset_dir)
+diseases = ['effusion', 'cardiomegaly', 'pneumothorax']
 
 
 def copy_images(disease):
+    """
+    Copies images with a disease from the list above from a the original dataset to a folder
+    with only 3 different diseases
+
+    INPUT
+        disease: The disease from which the images will be moved
+
+    OUTPUT
+        None
+    """
     skipped = 0
     number_of_images = 0
     directory = ROOT_DIR + '/Data/imagesK3' 
@@ -31,19 +40,20 @@ def copy_images(disease):
                     continue
     return skipped, number_of_images
 
-fields = ['Image Index','Finding Labels']
-data_frame = pandas.read_csv(os.path.abspath(data_entry), usecols=fields)
-data_dict = data_frame.set_index('Image Index').T.to_dict('list')
-images_per_disease = defaultdict(list)
-for k, v in data_dict.items():
-    images_per_disease[v[0].lower()].append(k)
+if __name__ == '__main__':
 
+    data_frame = pandas.read_csv(os.path.abspath(data_entry), usecols=fields)
+    data_dict = data_frame.set_index('Image Index').T.to_dict('list')
+    images_per_disease = defaultdict(list)
+    for k, v in data_dict.items():
+        images_per_disease[v[0].lower()].append(k)
 
-diseases = ['effusion', 'cardiomegaly', 'pneumothorax']
-for disease in diseases:
-    print("Copying images for:  " + disease)
-    skipped, number_of_images = copy_images(disease)
-    print("Number of images not found for : " + disease + " is " + str(skipped))
-    print("Number of images for : " + disease + " is " + str(number_of_images))
+    fields = ['Image Index','Finding Labels']
+
+    for disease in diseases:
+        print("Copying images for:  " + disease)
+        skipped, number_of_images = copy_images(disease)
+        print("Number of images not found for : " + disease + " is " + str(skipped))
+        print("Number of images for : " + disease + " is " + str(number_of_images))
 
 
