18911b10369b192df6a6e67a8c6a5930b196ba01
stephan.gui <stephan.gui4@gmail.com>
2020-04-09 17:51:14 +0200

    added use of command line and exceptionhandling

--- src/filter_diseases.py
+++ src/filter_diseases.py
@@ -3,13 +3,23 @@ import pandas
 from collections import defaultdict
 from shutil import copyfile
 import re
+from tkinter import Tk
+from tkinter.filedialog import *
+import ctypes  # An included library with Python install.
 
 ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..'))
 directory = f'{ROOT_DIR}'
-original_dataset_dir = f'/NAS/NIH_chest/ChestXray-NIHCC/images/'
-data_entry = os.path.join(directory, 'Data_Entry_2017.csv')
 diseases = ['effusion', 'cardiomegaly', 'pneumothorax']
 
+def open_dir():
+    Tk().withdraw()
+    file_path = askdirectory()
+    return file_path
+
+def open_dataset():
+    Tk().withdraw()
+    file_path = askopenfilename()
+    return file_path
 
 def copy_images(disease):
     """
@@ -41,19 +51,25 @@ def copy_images(disease):
     return skipped, number_of_images
 
 if __name__ == '__main__':
-
-    data_frame = pandas.read_csv(os.path.abspath(data_entry), usecols=fields)
-    data_dict = data_frame.set_index('Image Index').T.to_dict('list')
-    images_per_disease = defaultdict(list)
-    for k, v in data_dict.items():
-        images_per_disease[v[0].lower()].append(k)
+    ctypes.windll.user32.MessageBoxW(0, "Select image directory", "Selection menu", 1)
+    original_dataset_dir = open_dir()
+    ctypes.windll.user32.MessageBoxW(0, "Select dataset csv ", "Selection menu", 1)
+    data_entry = open_dataset()
 
     fields = ['Image Index','Finding Labels']
+    try:
+        data_frame = pandas.read_csv(os.path.abspath(data_entry), usecols=fields)
+        data_dict = data_frame.set_index('Image Index').T.to_dict('list')
+        images_per_disease = defaultdict(list)
+        for k, v in data_dict.items():
+            images_per_disease[v[0].lower()].append(k)
 
-    for disease in diseases:
-        print("Copying images for:  " + disease)
-        skipped, number_of_images = copy_images(disease)
-        print("Number of images not found for : " + disease + " is " + str(skipped))
-        print("Number of images for : " + disease + " is " + str(number_of_images))
 
+        for disease in diseases:
+            print("Copying images for:  " + disease)
+            skipped, number_of_images = copy_images(disease)
+            print("Number of images not found for : " + disease + " is " + str(skipped))
+            print("Number of images for : " + disease + " is " + str(number_of_images))
+    except pandas.errors.emptydataerror:
+        print("Csv file is empty, load new csv.")
 
